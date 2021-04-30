517a8a99a0f90992d32d85566f29b997a497a4dc
Mikhail Valkov <mikhail.valkov@gmail.com>
2021-04-30 23:24:18 +0300

    implement Input component

---
+++ src/components/Input.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import "../styles/Input.css";
+
+function Input({ label, type, size, isDisabled, value, onChangeHandler }) {
+  return (
+    <div className="Input-Wrapper">
+      <span className="Input-Label">{label}</span>
+      <input
+        className={classNames(
+          "Input-Field",
+          isDisabled && "Input-Field__disabled"
+        )}
+        onKeyUp={
+          type === "number" ? (e) => replaceAnyNonDigitSymbols(e) : undefined
+        }
+        min={type && "1"}
+        value={value && value}
+        size={size && size}
+        onChange={(e) => onChangeHandler(e.target.value.replaceAll(/\D/gm, ""))}
+      />
+    </div>
+  );
+}
+
+function replaceAnyNonDigitSymbols(e) {
+  e.target.value = e.target.value.replaceAll(/\D/gm, "");
+}
+
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  size: PropTypes.number,
+  isEditabele: PropTypes.bool,
+  value: PropTypes.number,
+  onChangeHandler: PropTypes.func,
+};
+
+export default Input;
