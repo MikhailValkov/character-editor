ca6a1cadcf5a2176701acf0d0371c60ae3439906
Mikhail Valkov <mikhail.valkov@gmail.com>
2021-04-30 14:36:29 +0300

    implement Frame component

---
+++ src/components/Frame.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/Frame.css";
+
+function Frame({ children, title }) {
+  return (
+    <div className="Frame">
+      {title && <span className="Frame-Title">{title}</span>}
+      {children}
+    </div>
+  );
+}
+
+Frame.propTypes = {
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+};
+
+export default Frame;
